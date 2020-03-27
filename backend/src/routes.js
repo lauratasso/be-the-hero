e228ebbd802d047a701033b405343cc5713191c2
Laura Tasso <laurabanhareli@hotmail.com>
2020-03-27 18:30:45 -0300

    back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ongController = require('./controllers/ong-controller');
+const incidentController = require('./controllers/incident-controller');
+const profileController = require('./controllers/profile-controller');
+const sessionController = require('./controllers/session-controller');
+
+
+const routes = express.Router();
+
+//a funcao assincrona se faz útil pelo insert poder demorar um pouco para ser concluido, então se 
+//faz assim e coloca um 'await' na funcao, que faz ele acabar de executar o insert para só depois retornar.
+
+routes.post('/session', sessionController.create);
+
+routes.post('/ongs', ongController.create);
+routes.get('/ongs', ongController.listAll);
+
+routes.post('/incidents', incidentController.create);
+routes.post('/incidents', incidentController.listAll);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.assignedIncidents);
+
+module.exports = routes;
